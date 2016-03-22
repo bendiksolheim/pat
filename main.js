caa2943cdf899bab00cecca5a317d82cf9f4ff83
Bendik Solheim <bendik@solheim.im>
2016-03-22 23:34:23 +0100

    Initial commit. Simple running version with build scripts working

---
+++ main.js
@@ -0,0 +1,21 @@
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+
+require('electron-reload')(__dirname);
+
+let mainWindow;
+
+app.on('ready', () => {
+    mainWindow = new BrowserWindow({
+        width: 1024,
+        height: 768,
+        'title-bar-style': 'hidden'
+    });
+    mainWindow.loadURL(`file://${__dirname}/static/index.html`);
+    mainWindow.openDevTools();
+
+    mainWindow.on('closed', () => {
+        mainWindow = null;
+    })
+});
