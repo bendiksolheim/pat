859765b3b09dabcfb77c5cb46b3075f6c63730a3
Bendik Solheim <bendik@solheim.im>
2016-03-24 20:49:28 +0100

    Track if there is a response or not

--- app/reducers/ajax.js
+++ app/reducers/ajax.js
@@ -3,6 +3,7 @@ import * as c from '../actions/constants';
 const initialState = {
     isFetching: false,
     failed: false,
+    hasResponse: false,
     response: {}
 };
 
@@ -15,10 +16,10 @@ function createResponse(body) {
 export default (state = initialState, action) => {
     switch(action.type) {
     case c.AJAX_REQUEST:
-        return Object.assign({}, state, {isFetching: true});
+        return Object.assign({}, state, {isFetching: true, hasResponse: false, failed: false});
     case c.AJAX_RECEIVE:
         const response = createResponse(action.body);
-        return Object.assign({}, state, {isFetching: false, response, failed: false});
+        return Object.assign({}, state, {isFetching: false, response, hasResponse: true});
     case c.AJAX_FAIL:
         return Object.assign({}, state, {isFetching: false, response: action.response, failed: true});
     default:
