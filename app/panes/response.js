ce78a8fe335bd327792c4246cd5ab89f763a4492
Bendik Solheim <bendik@solheim.im>
2016-03-27 21:20:25 +0200

    More sane styling of the response block

--- app/panes/response.js
+++ app/panes/response.js
@@ -15,8 +15,12 @@ function responseIcon(ok) {
 
 const Statistics = ({ajax}) => (
     <div className='response-stats'>
-        <span className={ responseIcon(ajax.response.ok) }></span><div className='response-stats__status'>{ajax.response.status}</div>
-        <span className="icon icon-clock"></span><div className='response-stats__time'>{ajax.end - ajax.start}ms</div>
+        <div className='response-stats__status'>
+            <span className={ `${responseIcon(ajax.response.ok)} response-stats__icon` }></span><div className='response-stats__value'>{ajax.response.status}</div>
+        </div>
+        <div className='response-stats__time'>
+            <span className='icon icon-clock response-stats__icon'></span><div className='response-stats__value'>{ajax.end - ajax.start}ms</div>
+        </div>
     </div>
 );
 
@@ -27,7 +31,9 @@ const NoRequest = () => (
 const JsonResponse = (ajax) => (
     <div>
         <Statistics ajax={ajax} />
-        <ObjectInspector data={ajax.responseBody} expandLevel={10} />
+        <div className='padded-more'>
+            <ObjectInspector data={ajax.responseBody} expandLevel={10} />
+        </div>
     </div>
 );
 
@@ -37,11 +43,7 @@ const Response = ({ ajax }) => {
         response = JsonResponse(ajax);
     }
 
-    return (
-        <div className='padded-more'>
-            {response}
-        </div>
-    );
+    return response;
 };
 
 export default connect(mapStateToProps)(Response);
